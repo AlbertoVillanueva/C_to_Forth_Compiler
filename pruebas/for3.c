8a3d54c589e26943adfe1868b74496c922c69cfa
AlbertoVillanueva <alberto.villanueva@protonmail.com>
2019-05-07 22:40:41 +0200

    changed a bit

--- pruebas/for3.c
+++ pruebas/for3.c
@@ -4,12 +4,11 @@ main(){
 	int k;
 	//@ cr
 	for (i = 0; i < 3; i=i+1){
-		printf("",i);
 		for (j = 0; j < 3; j=j+1){
-			puts("    ");
+			printf("",i);
+			puts(": ");
 			printf("",j);
-			//@ cr
-			puts("        ");
+			puts(": ");
 			for (k = 0; k < 3; k=k+1){
 				printf("",k);
 			}
